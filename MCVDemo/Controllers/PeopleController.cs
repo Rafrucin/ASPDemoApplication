7b77b45083c2a5d51e47d31f65c9b53629ef41ad
Rafał Ruciński <rafal_rucinski@poczta.fm>
2020-02-11 00:00:50 +0000

    ok

---
+++ MCVDemo/Controllers/PeopleController.cs
@@ -0,0 +1,29 @@
+﻿using MCVDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MCVDemo.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListPeople()
+        {
+            List<PersonModel> people = new List<PersonModel>();
+            people.Add(new PersonModel { FirstName = "Raf", LastName = "Ruci", Age = 40 });
+            people.Add(new PersonModel { FirstName = "Woj", LastName = "Ruc", Age = 15 });
+
+
+            
+            return View(people);
+        }
+    }
+}
